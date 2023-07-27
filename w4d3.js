cd2c305ce8825aeab196e798f39b2564d2f141e6
cyan-byte <62404444+cyan-byte@users.noreply.github.com>
2023-07-27 10:40:31 -0400

    Wild Wild Life answered

--- w4d3.js
+++ w4d3.js
@@ -74,15 +74,24 @@ for(let i = 1; i <= 100; i++){
 // Wild Wild Life
 // Use the following arrays to answer the questions below (name,species ,age, hometown): You should be modifying the elements by accessing them. It is up to you which methods to use.
 
-// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-// const sharky = ["Sharky", "shark", 20, "Left Coast"]
-// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
 // Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee.splice(2, 1, 5001)
+console.log(plantee)
 // Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy.splice(3, 1, 'Gotham City')
+console.log(wolfy)
 // Give D'Art a second hometown by adding "Hawkins"
+dart.push('Hawkins')
+console.log(dart)
 // Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+wolfy.splice(0, 1, 'Gameboy')
+console.log(wolfy)
 // 🔴 The commit message should read:
 // "Wild Wild Life answered"
 // Yell at the Ninja Turtles
